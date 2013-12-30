b5dc33a050f78bbeed7560caa7a4a9df2f4e35ec
root <root@liluxcast.localdomain>
2013-12-30 16:44:14 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9baefdf05101930455eef2144cf596ec73a77fe4479c8d040fcf5b715b75f6650ece3e24e095982eb5f9a2f2e43c2f9db3d34a11a546614b1692af0f5f8cee1c'
